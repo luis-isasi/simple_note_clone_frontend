eb6564e8907e5c7ac8cdd51443199bb8f03aa19b
Luis <luis16isasi@gmail.com>
2021-01-12 18:48:13 -0500

    webpack config ready

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const App = () => {
+  return <div>HOLA MUNDO CON REACT </div>;
+};
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import App from './App';
+
+const mainNode = document.querySelector('#root');
+
+ReactDOM.render(<App />, mainNode);
